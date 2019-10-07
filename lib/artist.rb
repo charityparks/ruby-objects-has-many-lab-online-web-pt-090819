fb6a9531cb2eef472099e1d7f95e18d619f0e113
Charity Parks <tuppercharity@gmail.com>
2019-10-07 21:40:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,10 @@
 class Artist 
   
-  attr_accessor :name, :songs
+  attr_accessor :name
   
-  def initialize(name, song)
+  def initialize(name)
     @name = name
-    @songs = []
+   # @songs = []
   end
   
   
